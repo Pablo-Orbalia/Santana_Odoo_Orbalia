a1b171c06812fbeca347e745c52faf31d2c69c3f
Pablo Santana <psantana@orbalia.es>
2025-10-03 12:44:44 +0100

    :zap: Creación del módulo Orbalia Base

---
+++ addons/orbalia_base/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Orbalia Base',
+    'version': '18.0.1.0.0',
+    'summary': 'Campos base y vistas para Contactos',
+    'author': 'Orbalia',
+    'website': 'https://orbalia.es',
+    'license': 'LGPL-3',
+    'category': 'Tools',
+    'depends': ['base', 'contacts'],
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+    'installable': True,
+    'application': False
+}
---
+++ addons/orbalia_base/models/res_partner.py
@@ -0,0 +1,5 @@
+﻿from odoo import fields, models
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+    es_empresa_operativa = fields.Boolean(string="Es empresa operativa")
